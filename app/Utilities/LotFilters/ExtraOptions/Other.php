017ab80003211b4281f42494186a10a63ce7070b
Valeriya_Tripolskaya <112164853+VSartVs@users.noreply.github.com>
2022-12-12 00:12:09 +0300

    test

--- app/Utilities/LotFilters/ExtraOptions/Other.php
+++ app/Utilities/LotFilters/ExtraOptions/Other.php
@@ -41,13 +41,13 @@ class Other extends SortQuery implements SortContract
         }
 
         if(!$value['isHidden'] && auth()->check()) {
-            //$this->query->whereNotIn('lots.id', auth()->user()->hiddenLots->pluck('id'));
+            $this->query->whereNotIn('lots.id', auth()->user()->hiddenLots->pluck('id'));
            // $this->query->doesntHave('userHiddenLot');
-            $this->query->whereNotExists(function ($query) {
+          /*  $this->query->whereNotExists(function ($query) {
                 $query->select(DB::raw(1))
                     ->from('hidden_lots')
                     ->whereRaw('hidden_lots.lot_id = lots.id AND user_id ='.auth()->id());
-            });
+            });*/
 
         }
         if(isset($minDate) && isset($maxDate)){
