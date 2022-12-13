02f2dcc237e75ab4b028500c0af01a293c44146c
Valeriya_Tripolskaya <112164853+VSartVs@users.noreply.github.com>
2022-12-13 12:01:59 +0300

    hot fix

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\ApplicationsController;
 use App\Http\Controllers\Admin\ContactsController;
+use App\Http\Controllers\Admin\ContentRulesController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\LotController;
 use App\Http\Controllers\Admin\RoleController;
@@ -405,6 +406,14 @@ Route::group(['middleware' => ['json.response', 'localization']], function () {
                     Route::delete('/{id}', [LotController::class, 'delete']);
 
                 });
+
+           /*     Route::group(['prefix' => 'content-rules'], function () {
+
+                    Route::get('/', [ContentRulesController::class, 'get']);
+
+                    Route::put('/update', [ContentRulesController::class, 'update']);
+
+                });*/
 
             });
 
